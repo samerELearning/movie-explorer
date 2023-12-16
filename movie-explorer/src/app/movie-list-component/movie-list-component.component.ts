18be5dd85a2b4f3041f247e63cf9cb662985b364
samerELearning <samerelearning@outlook.com>
2023-12-16 22:18:10 +0200

    Generated movie list component

---
+++ movie-explorer/src/app/movie-list-component/movie-list-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-list-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './movie-list-component.component.html',
+  styleUrl: './movie-list-component.component.css'
+})
+export class MovieListComponentComponent {
+
+}
