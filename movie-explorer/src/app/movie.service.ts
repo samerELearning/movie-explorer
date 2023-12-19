39f6a3eecdd4dc57b87966bec8392822211483b5
samerELearning <samerelearning@outlook.com>
2023-12-19 16:03:24 +0200

    Implemented getMovies method to fetch popular movies

--- movie-explorer/src/app/movie.service.ts
+++ movie-explorer/src/app/movie.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getMovies(): Observable<any> {
+    const apiKey = '8c899905c209b40ab22093a143df9412';
+    return this.http.get(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`);
+  }
 }
